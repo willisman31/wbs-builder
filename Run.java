718a6235d12bd3966622d7c5dc79fffc34ed063c
willisman31 <58826505+willisman31@users.noreply.github.com>
2021-04-09 08:29:48 -0400

    Add files via upload

---
+++ Run.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @author Willis
+ * @version 4.9.2021
+ */
+public class Run {
+
+	/**
+	 * Main method.
+	 * @param args accepts command line arguments.
+	 */
+	public static void main(String[] args) {
+		// TODO
+		runLocal();
+	}
+
+	/**
+	 * 
+	 */
+	private static void runLocal() {
+		
+	}
+	
+}
